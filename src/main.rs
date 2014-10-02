189f7e19a2596de68e764a860031214ce5ed6936
jxv <joevargas@hush.com>
2014-10-02 12:00:47 -0700

    rm fail! in 'impl Decodable for Board'; parse example json

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
 #![feature(globs)]
 extern crate serialize;
 use std::string::String;
+use std::io::File;
 use self::serialize::json;
 
 use vindinium::*;
 mod vindinium;
 
 
+
 fn main() {
-    println!("Hello, world!")
+    let contents = File::open(&Path::new("example.json")).read_to_string();
+    let data = match contents {
+        Ok(d) => d,
+        Err(_) => String::from_str(""),
+    };
+    // println!("{}", data);
 
     let settings = vindinium::Settings {
         key: String::from_str(""),
         url: String::from_str("")
     };
 
-    vindinium::request(&settings, "");
-    println!("{}", json::encode(&North));
+    match vindinium::request(&settings, data.as_slice()) {
+        None => (),
+        Some(s) => println!("{}",s),
+    } 
 
+    // println!("{}", json::encode(&North));
+
+/*
     {
         let j = "{\"x\":8, \"y\":9}";
         let res: json::DecodeResult<Pos> = json::decode(j);
@@ -26,7 +38,9 @@ fn main() {
             Err(err) => println!("err: {}",err),
         };
     }
+*/
 
+/*
     {
         let j = "{\"id\":1, \"name\":\"bobby\", \"userId\":\"bobbert\", \"elo\":10, \"pos\":{\"x\":10,\"y\":80}, \"life\":50, \"gold\":700, \"mineCount\":3, \"spawnPos\":{\"x\":8,\"y\":3}, \"crashed\":false}";
         let res: json::DecodeResult<Hero> = json::decode(j);
@@ -35,7 +49,8 @@ fn main() {
             Err(err) => println!("err: {}",err),
         };
     }
-
+*/
+/*
     {
         let j = "{\"size\":4, \"tiles\":\"@1  ##[]@2  $-$2@3  []  @4##  ##$-\"}";
         let res: json::DecodeResult<Board> = json::decode(j);
@@ -44,5 +59,5 @@ fn main() {
             Err(err) => println!("err: {}",err),
         };
     }
-
+*/
 }
--- src/vindinium.rs
+++ src/vindinium.rs
@@ -30,7 +30,7 @@ pub struct Game {
     pub id: GameId,
     pub turn: int,
     pub max_turns: int,
-    pub heroes: int,
+    pub heroes: Vec<Hero>,
     pub board: Board,
     pub finished: bool,
 }
@@ -57,8 +57,8 @@ pub struct Hero {
 
 #[deriving(Show)]
 pub struct Board {
-    pub size: int,
-    pub tiles: Vec<Tile>,
+    pub size: uint,
+    pub tiles: Vec<Vec<Tile>>,
 }
 
 #[deriving(Show)]
@@ -105,85 +105,98 @@ pub fn request(settings: &Settings, val: &str) -> Option<State> {
 // Json
 
 impl <S: Encoder<E>, E> Encodable<S, E> for Dir {
-    fn encode(&self, encoder: &mut S) -> Result<(), E> {
+    fn encode(&self, e: &mut S) -> Result<(), E> {
         match *self {
-            Stay =>  encoder.emit_str("Stay"),
-            North => encoder.emit_str("North"),
-            South => encoder.emit_str("South"),
-            East =>  encoder.emit_str("East"),
-            West =>  encoder.emit_str("West"),
+            Stay =>  e.emit_str("Stay"),
+            North => e.emit_str("North"),
+            South => e.emit_str("South"),
+            East =>  e.emit_str("East"),
+            West =>  e.emit_str("West"),
         }
     }
 }
 
 impl <S: Decoder<E>, E> Decodable<S, E> for Pos {
-    fn decode(decoder: &mut S) -> Result<Pos, E> {
-        decoder.read_struct("root", 0, |decoder| {
+    fn decode(d: &mut S) -> Result<Pos, E> {
+        d.read_struct("root", 0, |d| {
             Ok(Pos {
-                x: try!(decoder.read_struct_field("x", 0, |decoder| Decodable::decode(decoder))),
-                y: try!(decoder.read_struct_field("y", 1, |decoder| Decodable::decode(decoder))),
+                x: try!(d.read_struct_field("x", 0, |d| Decodable::decode(d))),
+                y: try!(d.read_struct_field("y", 1, |d| Decodable::decode(d))),
            })
         })
     }
 }
 
 impl <S: Decoder<E>, E> Decodable<S, E> for Hero {
-    fn decode(decoder: &mut S) -> Result<Hero, E> {
-        decoder.read_struct("root", 0, |decoder| {
+    fn decode(d: &mut S) -> Result<Hero, E> {
+        d.read_struct("root", 0, |d| {
             Ok(Hero {
-                id: try!(decoder.read_struct_field("id", 0, |decoder| Decodable::decode(decoder))),
-                name: try!(decoder.read_struct_field("name", 1, |decoder| Decodable::decode(decoder))),
-                user_id: try!(decoder.read_struct_field("userId", 2, |decoder| {
-                        Ok(match Decodable::decode(decoder) {
-                            Ok(user_id) => Some(user_id),
-                            Err(_) => None,
-                        })
-                    })),
-                elo: try!(decoder.read_struct_field("elo", 3, |decoder| {
-                        Ok( match Decodable::decode(decoder) {
-                            Ok(elo) => Some(elo),
-                            Err(_) => None,
-                        })
-                    })),
-                pos: try!(decoder.read_struct_field("pos", 4, |decoder| Decodable::decode(decoder))),
-                life: try!(decoder.read_struct_field("life", 5, |decoder| Decodable::decode(decoder))),
-                gold: try!(decoder.read_struct_field("gold", 6, |decoder| Decodable::decode(decoder))),
-                mine_count: try!(decoder.read_struct_field("mineCount", 7, |decoder| Decodable::decode(decoder))),
-                spawn_pos: try!(decoder.read_struct_field("spawnPos", 8, |decoder| Decodable::decode(decoder))),
-                crashed: try!(decoder.read_struct_field("crashed", 9, |decoder| Decodable::decode(decoder))),
+                id: try!(d.read_struct_field("id", 0, |d| Decodable::decode(d))),
+                name: try!(d.read_struct_field("name", 1, |d| Decodable::decode(d))),
+                user_id: try!(d.read_struct_field("userId", 2, |d| {
+                    Ok(match Decodable::decode(d) {
+                        Ok(user_id) => Some(user_id),
+                        Err(_) => None,
+                    })
+                })),
+                elo: try!(d.read_struct_field("elo", 3, |d| {
+                    Ok( match Decodable::decode(d) {
+                        Ok(elo) => Some(elo),
+                         Err(_) => None,
+                    })
+                })),
+                pos: try!(d.read_struct_field("pos", 4, |d| Decodable::decode(d))),
+                life: try!(d.read_struct_field("life", 5, |d| Decodable::decode(d))),
+                gold: try!(d.read_struct_field("gold", 6, |d| Decodable::decode(d))),
+                mine_count: try!(d.read_struct_field("mineCount", 7, |d| Decodable::decode(d))),
+                spawn_pos: try!(d.read_struct_field("spawnPos", 8, |d| Decodable::decode(d))),
+                crashed: try!(d.read_struct_field("crashed", 9, |d| Decodable::decode(d))),
             })
         })
     }
 }
 
 impl <S: Decoder<E>, E> Decodable<S, E> for Board {
-    fn decode(decoder: &mut S) -> Result<Board, E> {
-        let size = try!(decoder.read_struct_field("size", 0, |decoder| Decodable::decode(decoder)));
-        Ok(Board { size: size, tiles: try!(decoder.read_struct_field("tiles", 1, |decoder| {
-            let mut i = 0u;
-            let mut tiles = Vec::new();
-            let tile_str = try!(decoder.read_str());
+    fn decode(d: &mut S) -> Result<Board, E> {
+        let size = try!(d.read_struct_field("size", 0, |d| Decodable::decode(d)));
+        Ok(Board { size: size, tiles: try!(d.read_struct_field("tiles", 1, |d| {
+            let mut tiles: Vec<Vec<Tile>> = Vec::with_capacity(size);
+            let tile_str = try!(d.read_str());
+            if tile_str.len() != size * size * 2 {
+                return Err(d.error("tile string is incorrect size"));
+            }
             let tile_bytes = tile_str.as_bytes();
+            let mut i = 0u;
             loop {
+                let mut row: Vec<Tile> = Vec::with_capacity(size);
+                loop {
+                    if i >= tile_str.len() {
+                        break;
+                    }
+                    match (tile_bytes[i] as char, tile_bytes[i+1] as char) {
+                        (' ',' ') => row.push(FreeTile),
+                        ('#','#') => row.push(WoodTile),
+                        ('@',c)   => match to_digit(c,10) {
+                            None => return Err(d.error("failed parse HeroTile num")),
+                            Some(n) => row.push(HeroTile(n as int)),
+                        },
+                        ('[',']') => row.push(TavernTile),
+                        ('$','-') => row.push(MineTile(None)),
+                        ('$',c)   => match to_digit(c,10) {
+                            None => return Err(d.error("failed parse MineTile num")),
+                            Some(n) => row.push(MineTile(Some(n as int))),
+                        },
+                        (a,b) => return Err(d.error(format!("failed parsing tile \"{}{}\"", a, b).as_slice())),
+                    }
+                    i += 2;
+                    if i % (size * 2) == 0 {
+                        break;
+                    }
+                }
+                tiles.push(row);
                 if i >= tile_str.len() {
-                    break
+                    break;
                 }
-                match (tile_bytes[i] as char, tile_bytes[i+1] as char) {
-                    (' ',' ') => tiles.push(FreeTile),
-                    ('#','#') => tiles.push(WoodTile),
-                    ('@',c)   => match to_digit(c,10) {
-                        None => fail!("failed parse HeroTile num"),
-                        Some(n) => tiles.push(HeroTile(n as int)),
-                    },
-                    ('[',']') => tiles.push(TavernTile),
-                    ('$','-') => tiles.push(MineTile(None)),
-                    ('$',c)   => match to_digit(c,10) {
-                        None => fail!("failed parse MineTile num"),
-                        Some(n) => tiles.push(MineTile(Some(n as int))),
-                    },
-                    (a,b) => fail!("failed parsing tile \"{}{}\"", a, b),
-                }
-                i += 2;
             }
             Ok(tiles)
         }))})
@@ -191,26 +204,26 @@ impl <S: Decoder<E>, E> Decodable<S, E> for Board {
 }
 
 impl <S: Decoder<E>, E> Decodable<S, E> for Game {
-    fn decode(decoder: &mut S) -> Result<Game, E> {
+    fn decode(d: &mut S) -> Result<Game, E> {
         Ok(Game {
-            id: try!(decoder.read_struct_field("id", 0, |decoder| Decodable::decode(decoder))),
-            turn: try!(decoder.read_struct_field("turn", 1, |decoder| Decodable::decode(decoder))),
-            max_turns: try!(decoder.read_struct_field("maxTurns",2, |decoder| Decodable::decode(decoder))),
-            heroes: try!(decoder.read_struct_field("heroes", 3, |decoder| Decodable::decode(decoder))),
-            board: try!(decoder.read_struct_field("board", 4, |decoder| Decodable::decode(decoder))),
-            finished: try!(decoder.read_struct_field("finished", 5, |decoder| Decodable::decode(decoder))),
+            id: try!(d.read_struct_field("id", 0, |d| Decodable::decode(d))),
+            turn: try!(d.read_struct_field("turn", 1, |d| Decodable::decode(d))),
+            max_turns: try!(d.read_struct_field("maxTurns",2, |d| Decodable::decode(d))),
+            heroes: try!(d.read_struct_field("heroes", 3, |d| Decodable::decode(d))),
+            board: try!(d.read_struct_field("board", 4, |d| Decodable::decode(d))),
+            finished: try!(d.read_struct_field("finished", 5, |d| Decodable::decode(d))),
         })
     }
 }
 
 impl <S: Decoder<E>, E> Decodable<S, E> for State {
-    fn decode(decoder: &mut S) -> Result<State, E> {
+    fn decode(d: &mut S) -> Result<State, E> {
         Ok(State {
-            game: try!(decoder.read_struct_field("game", 0, |decoder| Decodable::decode(decoder))),
-            hero: try!(decoder.read_struct_field("hero", 0, |decoder| Decodable::decode(decoder))),
-            token: try!(decoder.read_struct_field("token", 0, |decoder| Decodable::decode(decoder))),
-            view_url: try!(decoder.read_struct_field("viewUrl", 0, |decoder| Decodable::decode(decoder))),
-            play_url: try!(decoder.read_struct_field("playUrl", 0, |decoder| Decodable::decode(decoder))),
+            game: try!(d.read_struct_field("game", 0, |d| Decodable::decode(d))),
+            hero: try!(d.read_struct_field("hero", 1, |d| Decodable::decode(d))),
+            token: try!(d.read_struct_field("token", 2, |d| Decodable::decode(d))),
+            view_url: try!(d.read_struct_field("viewUrl", 3, |d| Decodable::decode(d))),
+            play_url: try!(d.read_struct_field("playUrl", 4, |d| Decodable::decode(d))),
         })
     }
 }
