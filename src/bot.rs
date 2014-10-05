411376beef752978fa8e3c38b1766949073502c6
jxv <joevargas@hush.com>
2014-10-05 14:05:38 -0700

    rm hero_id from RandomBot

--- src/bot.rs
+++ src/bot.rs
@@ -4,14 +4,12 @@ use vindinium::{Bot, Dir, Stay, State, HeroId};
 
 #[deriving(Show,Clone)]
 pub struct RandomBot {
-    pub hero_id: HeroId,
     pub dir: Dir,
 }
 
 impl RandomBot {
     pub fn new() -> RandomBot {
         RandomBot {
-            hero_id: 0,
             dir: Stay,
         }
     }
@@ -20,9 +18,7 @@ impl RandomBot {
 impl Bot for RandomBot {
 
     fn step(&self, state: &State) -> RandomBot {
-        println!("{}\n\n", state);
         let mut bot: RandomBot = self.clone();
-        bot.hero_id = state.hero.id;
         bot.dir = rand::random();
         bot 
     }
